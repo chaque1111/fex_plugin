eae33d0c33c4117033f2f26edc3e7594eec007bb
chaque1111 <99604927+chaque1111@users.noreply.github.com>
2023-09-04 18:24:20 -0300

    modificando css y html de los horarios de envío

--- components/shipping-times/shipping-times.php
+++ components/shipping-times/shipping-times.php
@@ -27,44 +27,51 @@ include_once "times-functions.php";
 </div>
 
 <div class="container">
-    <h1>Optimiza tus Envíos con Fex: Configuración de Horarios</h1>
-    <p>Para brindarte la mejor experiencia de entrega, sabemos lo importante que es sincronizar tus horarios con los
-        nuestros. Por eso, hemos diseñado un sistema que se adapta perfectamente a tu disponibilidad.</p>
-    <p>Ésto nos ayuda a validar que los clientes de tu tienda no puedan programar envíos en horarios que tu tienda no
-        esté operando y que Fex pase a recoger pedidos en los horarios correctos.</p>
-    <div class="highlight">
-        <p><strong>Horario de Envío Sincronizado:</strong> 8am - 10pm</p>
-        <p>Nuestro horario de envío está diseñado para trabajar en sintonía con tus necesidades. Si tu tienda o
-            ubicación de envío está operativa entre las 8:00 a.m. y las 10:00 p.m., estás de suerte. Hemos configurado
-            de manera predeterminada este horario para tus envíos. Esto significa que no necesitas realizar ninguna
-            configuración adicional si tus operaciones están dentro de este intervalo de tiempo.</p>
+    <div class="title-container">
+        <h1 class="time-title">Configuración de Horarios</h1>
+        <p>Para brindarte la mejor experiencia de entrega, sabemos lo importante que es sincronizar tus horarios con los
+            nuestros. Por eso, hemos diseñado un sistema que se adapta perfectamente a tu disponibilidad.</p>
+        <p class="fex-times"><strong>Horario de Envío Fex:</strong> 08:00 AM - 22:00 PM</p>
+        <p><strong>¿Cómo Funciona?</strong><br>Si tu tienda no opera durante todo este horario, puedes
+            especificar el
+            horario en que comienza a operar y el
+            horario en el que termina, cuando guardes la configuración nuestro sistema ajustará los horarios de recogida
+            y
+            entrega en función de este
+            rango. Puedes confiar en que tus envíos serán gestionados de manera eficiente y llegarán a su destino en los
+            momentos óptimos.</p>
+        <p>Ésta configuración nos ayuda a validar que los clientes de tu tienda Woo no puedan programar envíos en
+            horarios
+            que tu tienda
+            no
+            esté operando y que Fex pase a recoger pedidos en los horarios correctos.</p>
+
     </div>
-    <p><strong>¿Cómo Funciona?</strong><br>Si tu tienda está disponible para operaciones entre las 8:00 a.m. y las 10:00
-        p.m., nuestro sistema de envíos automáticamente ajustará los horarios de recogida y entrega en función de este
-        rango. Puedes confiar en que tus envíos serán gestionados de manera eficiente y llegarán a su destino en los
-        momentos óptimos.</p>
 
-    <p>Si tu tienda no opera durante todo este horario, puedes especificar el horario en que comienza a operar y el
-        horario en el que termina:</p>
-    <p>Recuerda debes elegir horarios entre el rango de 8am y 22pm</p>
-    <form method="post">
-        <?php
-        if (get_option("shipping_times_is_config")) {
-            echo '<label for="horaInicio">Hora de Inicio:</label>';
-            echo '<input type="time" id="horaInicio" name="horaInicio" min="08:00" max="22:00" value=' . get_option("shipping_times_min") . ' required ><br><br> ';
-            echo '<label for="horaFin">Hora de Fin:</label>';
-            echo '<input type="time" id="horaFin" name="horaFin" min="08:00"  value=' . get_option("shipping_times_max") . ' max="22:00" required><br><br>';
-        }
-        else {
-            echo '<label for="horaInicio">Hora de Inicio:</label>';
-            echo '<input type="time" id="horaInicio" name="horaInicio" min="08:00" max="22:00" required><br><br>';
-            echo '<label for="horaFin">Hora de Fin:</label>';
-            echo '<input type="time" id="horaFin" name="horaFin" min="08:00" max="22:00" required><br><br>';
-        }
-        ?>
+    <div class="highlight">
+        <p><strong>Horario de Envío Fex:</strong> 08:00 AM - 22:00 PM</p>
+        <p>Si tu horario de atención coincide con el de Fex (08:00 - 22:00), deja los valores por defecto.</p>
+    </div>
 
-
-        <button type="submit" class="button">Guardar Horarios</button>
+    <form class="form-times" method="post">
+        <div class="contain-inputs">
+            <?php
+            if (get_option("shipping_times_is_config")) {
+                echo '<label for="horaInicio">Hora de Inicio:</label>';
+                echo '<input type="time" id="horaInicio" name="horaInicio" min="08:00" max="22:00" value=' . get_option("shipping_times_min") . ' required ><br><br> ';
+                echo '<label for="horaFin">Hora de Fin:</label>';
+                echo '<input type="time" id="horaFin" name="horaFin" min="08:00"  value=' . get_option("shipping_times_max") . ' max="22:00" required><br><br>';
+            }
+            else {
+                echo '<label for="horaInicio">Hora de Inicio:</label>';
+                echo '<input type="time" id="horaInicio" name="horaInicio" min="08:00" max="22:00" value="08:00" required><br><br>';
+                echo '<label for="horaFin">Hora de Fin:</label>';
+                echo '<input type="time" id="horaFin" name="horaFin" min="08:00" max="22:00" value="22:00" required><br><br>';
+            }
+            ?>
+        </div>
+        <p class="alert-times">¡Si los horarios ingresados son correctos presiona el botón "Guardar Horarios"!</p>
+        <button type="submit" class="submit-times">Guardar Horarios</button>
     </form>
 </div>
 
